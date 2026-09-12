fe9b8f6241064af6e9a9073edf91f6bad711ea57
theparadoxShin <tedom.tedom7@gmail.com>
2026-09-12 06:00:20 +0000

    Stop tracking .cache

--- .cache/sketch/libraries/Sensirion Core/SensirionCrc.cpp.libsdetect.d
+++
@@ -1,5 +0,0 @@
-SensirionCrc.o: \
- /home/arduino/.arduino15/internal/Sensirion_Core_0.7.3_37f2af3180c2bb1d/Sensirion\ Core/src/SensirionCrc.cpp \
- /home/arduino/.arduino15/packages/arduino/hardware/zephyr/0.90.0/variants/arduino_uno_q_stm32u585xx/llext-edk/include/zephyr/include/generated/zephyr/autoconf.h \
- /home/arduino/.arduino15/packages/arduino/hardware/zephyr/0.90.0/variants/arduino_uno_q_stm32u585xx/llext-edk/include/zephyr/include/zephyr/toolchain/zephyr_stdint.h \
- /home/arduino/.arduino15/internal/Sensirion_Core_0.7.3_37f2af3180c2bb1d/Sensirion\ Core/src/SensirionCrc.h
